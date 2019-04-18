600f6000a38b77d1c53b1b1c6dbd629c0f3d9906
visualstudio <visualstudio@230-PC15>
2019-04-18 14:45:06 -0400

    tre

--- assignment5/Program.cs
+++ assignment5/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace assignment5
+namespace FrequencyOfArray
 {
     class Program
     {
-        static void Main(string[] args)
+        public int[] inputArray = new int[10] { 3, 11, 9, 11, 4, 11, 5, 9, 2, 1 };
+
+        static void Main(string[] args) { new Program().Run(); }
+
+        public void Run() { this.InterateOverArray(); }
+
+        public void InterateOverArray()
         {
+            int output;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                output = inputArray[i]; 
+
+                Console.WriteLine(Convert.ToString(output));
+            }
         }
     }
 }
